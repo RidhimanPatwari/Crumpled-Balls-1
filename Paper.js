344513356f2681892460afa2326779b9434203c0
RidhimanPatwari <65568269+RidhimanPatwari@users.noreply.github.com>
2021-02-17 18:30:46 -0800

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,55 @@
+class Paper {
+    constructor(x, y, r) {
+        var options = {
+            isStatic: false,
+            restitution: 0.3,
+            friction: 0.5,
+            density: 1.2
+        }
+        
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.body = Bodies.circle(this.x, this.y, this.r/2, options);
+        World.add(world, this.body);
+
+    }
+
+    display() {
+
+        var pos = this.body.position;
+
+        push();
+        translate(pos.x, pos.y);
+        strokeWeight(3);
+        stroke("black");
+        fill(255, 0, 255);
+        ellipseMode(CENTER);
+        ellipse(0, 0, this.r, this.r);
+        pop();
+
+    }
+
+}
+
+/*class Paper {
+    constructor(x, y, r) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.r = r;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        pop();
+      }
+}*/
---
+++ sketch.js
@@ -0,0 +1,52 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var paper, dustbin, ground; 
+
+function preload(){
+	
+}
+
+function setup() {
+	createCanvas(1600, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+	dustbin = new Dustbin(1200, 650); 
+	paper = new Paper(200, 450, 40);
+	ground = new Ground(width/2, 670, width, 20);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+	rectMode(CENTER);
+	background("black");
+
+	Engine.update(engine); 
+
+	paper.display();
+
+	dustbin.display(); 
+	ground.display();
+
+	drawSprites();
+ 
+}
+
+function keyPressed(){ 
+
+	if (keyCode === UP_ARROW){ 
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:85, y:-85});
+	}
+}
+
+
+
